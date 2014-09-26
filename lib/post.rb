596d5ff301338ee2ca3eea9e151e2d6642e154d4
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-25 17:49:52 -0700

    extensively change design.css

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,4 @@
-require 'sinatra'
-
-class Post < Sinatra::Base
-  # should be able to Access points, which are files within views/posts
-  # title, url/path, filename, content
-
-  # read my list of post files
-  # create an array of Post objects
-  # with the attributes set
-  # Post.all => [post, post]
+class Post 
 
   attr_accessor :title, :url
 
@@ -17,12 +8,10 @@ class Post < Sinatra::Base
   end
 
   def self.all
-    #Dir.glob("*") <---finds everything in the current directory
-    #Dir.glob("views/*")
-    # ** two splats: go recursively
     Dir.glob("views/posts/*").collect do |filepath|
       a = filepath.split("/")
-      self.new(a)
+      new(a)
     end
   end
+
 end
--- my_app.rb
+++ my_app.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
+require './lib/post'
 
 class MyApp < Sinatra::Base
 
   get "/" do
+    @posts = Post.all
+    @title = "SKWAK@ADA"
     erb :index
   end
 
-  get "/about-me" do
-    "skwak about me"
+  get "/about" do
+    @title = "SKWAK@ADA BIO"
+    erb :about
   end
 
+  get "/posts/:name" do
+    @posts = Post.all
+    @title = "SKWAK@ADA BLOG"
+    erb "/posts/#{params[:name]}".to_sym
+  end
+
+  # get "/posts/boom" do
+  #   @title = "SKWAK@ADA BOOM"
+  #   erb "/posts/boom".to_sym
+  # end
 end
