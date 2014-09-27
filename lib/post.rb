ddaa14d400be826485795f7bfe053e5261062198
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-26 19:15:05 -0700

    add self.recent_posts(n) method and made filler blog posts to test out function

--- lib/post.rb
+++ lib/post.rb
@@ -2,22 +2,22 @@ require "date"
 
 class Post
 
-  attr_accessor :title, :url, :day, :month, :year
+  attr_accessor :title, :url, :date
 
   def initialize(array)
     @title = array[-1].split(".")[0]
-    @url = "/posts/#{@title}"
+    @date = Date.parse(array[-2])
+    @url = "/posts/#{@date}/#{@title}"
   end
 
   def self.all
-    Dir.glob("views/posts/*").collect do |filepath|
+    Dir.glob("views/posts/*/*").collect do |filepath|
       a = filepath.split("/")
       new(a)
     end
   end
 
-  def show_recent
+  def self.most_recent(n)
+    all.sort_by { |post| post.date }.last(n)
   end
-
-
 end
--- my_app.rb
+++ my_app.rb
@@ -4,24 +4,20 @@ require './lib/post'
 class MyApp < Sinatra::Base
 
   get "/" do
-    @posts = Post.all
+    @posts = Post.most_recent(5)
     @title = "SKWAK@ADA"
     erb :index
   end
+
 
   get "/about" do
     @title = "SKWAK@ADA BIO"
     erb :about
   end
 
-  get "/posts/:title" do
-    # @posts = Post.all
+  get "/posts/:date/:title" do
     @title = "SKWAK@ADA BLOG"
-    erb "/posts/#{params[:title]}".to_sym
+    erb "/posts/#{params[:date]}/#{params[:title]}".to_sym
   end
 
-  # get "/posts/boom" do
-  #   @title = "SKWAK@ADA BOOM"
-  #   erb "/posts/boom".to_sym
-  # end
 end
