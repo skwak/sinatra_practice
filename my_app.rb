1fd5cef596a48242eb08b2a92dfec4325a602b61
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-30 09:43:34 -0700

    clean up my_app.rb with a before do

--- my_app.rb
+++ my_app.rb
@@ -3,24 +3,23 @@ require "./lib/post"
 
 class MyApp < Sinatra::Base
 
-  get "/" do
+  before do
     @recent_posts = Post.most_recent(5)
     @posts = Post.all
+  end
+
+  get "/" do
     @title = "SKWAK@ADA BLOG"
     erb :index
   end
 
   get "/about" do
-    @recent_posts = Post.most_recent(5)
-    @posts = Post.all
     @title = "SKWAK@ADA BIO"
     erb :about
   end
 
   get "/posts/:date/:title" do
-    @title = "SKWAK@ADA BLOG"
-    @recent_posts = Post.most_recent(5)
-    @posts = Post.all
+    @title = "SKWAK@ADA BLOG POST"
     erb "/posts/#{params[:date]}/#{params[:title]}".to_sym
   end
 
