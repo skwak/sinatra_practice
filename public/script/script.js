62fe44aff2c21e1b3e856f22c8a7005eb4b06971
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-10-01 14:31:07 -0700

    add jquery slideToggle() - unnecessary, but it works

---
+++ public/script/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var each_post = $(".content");
+    $(each_post).hide("slow");
+    $(".blog-title").click(function() {
+    $(this).next(each_post).slideToggle("slow");
+  });
+});
