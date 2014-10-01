f39d562a47e7a52dd790c860fdc75b561edea4c4
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-10-01 14:47:20 -0700

    made changes to elements related to slideToggle() jQuery behavior

--- public/script/script.js
+++ public/script/script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     var each_post = $(".content");
     $(each_post).hide("slow");
     $(".blog-title").click(function() {
-    $(this).next(each_post).slideToggle("slow");
+      $(this).next(each_post).slideToggle("slow");
   });
 });
