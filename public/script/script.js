827d2ae707965c7efd1bceccc5f2107255a80b3b
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-10-01 19:26:05 -0700

    typos in blog post fixed, seeing if I can get slideToggle() to work only in index

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,34 @@
+
+
 $(document).ready(function() {
-    var each_post = $(".content");
+  var each_post = $(".content");
+  // if (!$('div#home').length) return;
     $(each_post).hide("slow");
-    $(".blog-title").click(function() {
+    $(".blog-title").click(function(event) {
+      event.preventDefault();
       $(this).next(each_post).slideToggle("slow");
-  });
+
+
+  // } else {
+  //     $(".hide").css("display", "block");
+  //   $(each_post).show();
+  // }
+
+
 });
+});
+// $('.thumb').addClass('new');
+// callmeman();
+//
+//  $('#Button').click(function () {
+//      $('.thumb').removeClass('new');
+//      callmeman();
+//  });
+//
+//  function callmeman(){
+//      if ($('.thumb').hasClass('new')) {
+//          $('.info-top').hide();
+//       } else {
+//          $('.info-top').show();
+//      }
+//  }
